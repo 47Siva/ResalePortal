d349f8f648b73f3be78123f72b03b9c9a30bc9f1
47Siva <sivakalaikalai7@gmail.com>
2024-03-28 10:58:26 +0530

    frist commit

---
+++ src/main/java/com/demo/ResalePortal/ResalePortalApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ResalePortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResalePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResalePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/ResalePortal/ResalePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ResalePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResalePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
